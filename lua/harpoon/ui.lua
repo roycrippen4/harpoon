013f56c8e01f7704732a5b014f6b41f9ee8a824b
roy.crippen4 <roy.crippen4@archarithms.com>
2024-11-11 00:27:04 -0500

    wip: major refactor

--- lua/harpoon/ui.lua
+++ lua/harpoon/ui.lua
@@ -3,47 +3,9 @@ local Marked = require('harpoon.mark')
 local utils = require('harpoon.utils')
 local log = require('harpoon.dev').log
 
-local cmd = vim.cmd
-
-local fs = vim.fs
-local normalize = fs.normalize
-
-local fn = vim.fn
-local fnamemodify = fn.fnamemodify
-local is_dir = fn.isdirectory
-local sign_define = fn.sign_define
-local sign_place = fn.sign_place
-local bufexists = fn.bufexists
-local bufadd = fn.bufadd
-local getbufinfo = fn.getbufinfo
-
-local api = vim.api
-local autocmd = api.nvim_create_autocmd
-local create_namespace = api.nvim_create_namespace
-local list_uis = api.nvim_list_uis
-local set_option = api.nvim_set_option_value
-
-local get_current_win = api.nvim_get_current_win
-local set_current_win = api.nvim_set_current_win
-local win_open = api.nvim_open_win
-local win_close = api.nvim_win_close
-local win_is_valid = api.nvim_win_is_valid
-
-local buf_create = api.nvim_create_buf
-local buf_delete = api.nvim_buf_delete
-local buf_get_lines = api.nvim_buf_get_lines
-local buf_set_lines = api.nvim_buf_set_lines
-local buf_get_name = api.nvim_buf_get_name
-local buf_set_name = api.nvim_buf_set_name
-local buf_clear_ns = api.nvim_buf_clear_namespace
-local buf_set_keymap = api.nvim_buf_set_keymap
-local buf_is_loaded = api.nvim_buf_is_loaded
-local get_current_buf = api.nvim_get_current_buf
-local set_current_buf = api.nvim_set_current_buf
-
 local M = {}
 
-local nsid = create_namespace('harpoon')
+local nsid = vim.api.nvim_create_namespace('harpoon')
 Harpoon_win_id = nil
 Harpoon_bufh = nil
 
@@ -57,7 +19,7 @@ local function close_menu(force_save)
     require('harpoon.ui').on_menu_save()
   end
 
-  win_close(Harpoon_win_id, true)
+  vim.api.nvim_win_close(Harpoon_win_id, true)
 
   Harpoon_win_id = nil
   Harpoon_bufh = nil
@@ -70,7 +32,7 @@ local function create_window()
   local height = config.height or 10
   local borderchars = config.borderchars or { '╭', '─', '╮', '│', '╯', '─', '╰', '│' }
 
-  local bufnr = buf_create(false, false)
+  local bufnr = vim.api.nvim_create_buf(false, false)
 
   ---@type [string, string][]
   local border = vim
@@ -80,7 +42,7 @@ local function create_window()
     end)
     :totable()
 
-  local win_id = win_open(bufnr, true, {
+  local win_id = vim.api.nvim_open_win(bufnr, true, {
     relative = 'editor',
     style = 'minimal',
     width = width,
@@ -100,7 +62,7 @@ end
 
 local function get_menu_items()
   log.trace('_get_menu_items()')
-  local lines = buf_get_lines(Harpoon_bufh, 0, -1, true)
+  local lines = vim.api.nvim_buf_get_lines(Harpoon_bufh, 0, -1, true)
   local indices = {}
 
   for _, line in pairs(lines) do
@@ -114,28 +76,27 @@ end
 
 ---@param file_name string
 ---@return string
------@return string, string -- icon, hl_group
 local function get_sign(file_name)
-  if is_dir(file_name) == 1 then
+  if vim.fn.isdirectory(file_name) == 1 then
     local sign_name = 'HarpoonDirectory'
-    sign_define(sign_name, { text = '', texthl = 'Normal' })
+    vim.fn.sign_define(sign_name, { text = '', texthl = 'Normal' })
     return sign_name
   end
 
   local devicons = require('nvim-web-devicons')
-  local extension = fnamemodify(file_name, ':e')
+  local extension = vim.fn.fnamemodify(file_name, ':e')
   local icon, hl_group = devicons.get_icon(file_name, extension, { default = true })
   local sign_name = 'Harpoon' .. extension:upper()
-  sign_define(sign_name, { text = icon, texthl = hl_group })
+  vim.fn.sign_define(sign_name, { text = icon, texthl = hl_group })
 
   return sign_name
 end
 
 --- Sets the icon and it's highlight group
 local function draw_signs()
-  cmd('setlocal statuscolumn=%l%=%s')
-  local lines = buf_get_lines(Harpoon_bufh, 0, -1, true)
-  buf_clear_ns(Harpoon_bufh, nsid, 0, -1)
+  vim.cmd('setlocal statuscolumn=%l%=%s')
+  local lines = vim.api.nvim_buf_get_lines(Harpoon_bufh, 0, -1, true)
+  vim.api.nvim_buf_clear_namespace(Harpoon_bufh, nsid, 0, -1)
 
   if #lines == 1 and #lines[1] == 0 then
     return
@@ -144,7 +105,7 @@ local function draw_signs()
   for idx, _ in pairs(lines) do
     local file_name = Marked.get_marked_file_name(idx)
     local sign_name = get_sign(file_name)
-    sign_place(0, '', sign_name, Harpoon_bufh, { lnum = idx })
+    vim.fn.sign_place(0, '', sign_name, Harpoon_bufh, { lnum = idx })
   end
 end
 
@@ -165,22 +126,22 @@ end
 local function create_autocmds()
   local global_config = harpoon.get_global_settings()
 
-  autocmd('BufWriteCmd', {
+  vim.api.nvim_create_autocmd('BufWriteCmd', {
     buffer = Harpoon_bufh,
     callback = function()
       require('harpoon.ui').on_menu_save()
     end,
   })
 
-  autocmd('BufModifiedSet', {
+  vim.api.nvim_create_autocmd('BufModifiedSet', {
     buffer = Harpoon_bufh,
     callback = function()
-      cmd('set nomodified')
+      vim.cmd('set nomodified')
     end,
   })
 
   if global_config.save_on_change then
-    autocmd({ 'TextChanged', 'TextChangedI' }, {
+    vim.api.nvim_create_autocmd({ 'TextChanged', 'TextChangedI' }, {
       buffer = Harpoon_bufh,
       callback = function()
         require('harpoon.ui').on_menu_save()
@@ -189,7 +150,7 @@ local function create_autocmds()
     })
   end
 
-  autocmd('BufLeave', {
+  vim.api.nvim_create_autocmd('BufLeave', {
     nested = true,
     once = true,
     callback = function()
@@ -200,13 +161,13 @@ end
 
 function M.toggle_quick_menu()
   log.trace('toggle_quick_menu()')
-  if Harpoon_win_id ~= nil and win_is_valid(Harpoon_win_id) then
+  if Harpoon_win_id ~= nil and vim.api.nvim_win_is_valid(Harpoon_win_id) then
     close_menu()
     return
   end
 
-  local curr_file = utils.normalize_path(buf_get_name(0))
-  cmd(
+  local curr_file = utils.normalize_path(vim.api.nvim_buf_get_name(0))
+  vim.cmd(
     string.format(
       'autocmd Filetype harpoon '
         .. "let path = '%s' | call clearmatches() | "
@@ -224,22 +185,22 @@ function M.toggle_quick_menu()
   Harpoon_win_id = win_info.win_id
   Harpoon_bufh = win_info.bufnr
 
-  set_option('number', true, { win = Harpoon_win_id })
-  buf_set_name(Harpoon_bufh, 'harpoon-menu')
-  buf_set_lines(Harpoon_bufh, 0, #contents, false, contents)
-  set_option('filetype', 'harpoon', { buf = Harpoon_bufh })
-  set_option('buftype', 'acwrite', { buf = Harpoon_bufh })
-  set_option('bufhidden', 'delete', { buf = Harpoon_bufh })
-  buf_set_keymap(Harpoon_bufh, 'n', 'q', "<Cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>", { silent = true })
-  buf_set_keymap(Harpoon_bufh, 'n', '<ESC>', "<Cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>", { silent = true })
-  buf_set_keymap(Harpoon_bufh, 'n', '<CR>', "<Cmd>lua require('harpoon.ui').select_menu_item()<CR>", {})
+  vim.api.nvim_set_option_value('number', true, { win = Harpoon_win_id })
+  vim.api.nvim_buf_set_name(Harpoon_bufh, 'harpoon-menu')
+  vim.api.nvim_buf_set_lines(Harpoon_bufh, 0, #contents, false, contents)
+  vim.api.nvim_set_option_value('filetype', 'harpoon', { buf = Harpoon_bufh })
+  vim.api.nvim_set_option_value('buftype', 'acwrite', { buf = Harpoon_bufh })
+  vim.api.nvim_set_option_value('bufhidden', 'delete', { buf = Harpoon_bufh })
+  vim.api.nvim_buf_set_keymap(Harpoon_bufh, 'n', 'q', "<Cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>", { silent = true })
+  vim.api.nvim_buf_set_keymap(Harpoon_bufh, 'n', '<ESC>', "<Cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>", { silent = true })
+  vim.api.nvim_buf_set_keymap(Harpoon_bufh, 'n', '<CR>', "<Cmd>lua require('harpoon.ui').select_menu_item()<CR>", {})
 
   create_autocmds()
   draw_signs()
 end
 
 function M.select_menu_item()
-  local idx = fn.line('.')
+  local idx = vim.fn.line('.')
   close_menu(true)
   M.nav_file(idx)
 end
@@ -250,12 +211,12 @@ function M.on_menu_save()
 end
 
 local function get_or_create_buffer(filename)
-  local buf_exists = bufexists(filename) ~= 0
+  local buf_exists = vim.fn.bufexists(filename) ~= 0
   if buf_exists then
-    return fn.bufnr(filename)
+    return vim.fn.bufnr(filename)
   end
 
-  return bufadd(filename)
+  return vim.fn.bufadd(filename)
 end
 
 function M.nav_file(id)
@@ -271,45 +232,45 @@ function M.nav_file(id)
 
   if vim.bo.ft == 'NvimTree' then
     nvim_tree = true
-    cmd('NvimTreeClose')
+    vim.cmd('NvimTreeClose')
   end
 
   if vim.bo.ft == 'logger' then
     if #vim.api.nvim_list_wins() == 1 then
-      cmd('vsplit')
+      vim.cmd('vsplit')
     end
     logger = true
     require('logger'):close()
   end
 
   local mark = Marked.get_marked_file(idx)
-  local filename = normalize(mark.filename)
+  local filename = vim.fs.normalize(mark.filename)
   local buf_id = get_or_create_buffer(filename)
-  local set_row = not buf_is_loaded(buf_id)
+  local set_row = not vim.api.nvim_buf_is_loaded(buf_id)
 
-  local old_bufnr = get_current_buf()
+  local old_bufnr = vim.api.nvim_get_current_buf()
 
-  set_current_buf(buf_id)
-  set_option('buflisted', true, { buf = buf_id })
+  vim.api.nvim_set_current_buf(buf_id)
+  vim.api.nvim_set_option_value('buflisted', true, { buf = buf_id })
   if set_row and mark.row and mark.col then
-    cmd(string.format(':call cursor(%d, %d)', mark.row, mark.col))
+    vim.cmd(string.format(':call cursor(%d, %d)', mark.row, mark.col))
     log.debug(string.format('nav_file(): Setting cursor to row: %d, col: %d', mark.row, mark.col))
   end
 
-  local old_bufinfo = getbufinfo(old_bufnr)
+  local old_bufinfo = vim.fn.getbufinfo(old_bufnr)
   if type(old_bufinfo) == 'table' and #old_bufinfo >= 1 then
     old_bufinfo = old_bufinfo[1]
     local no_name = old_bufinfo.name == ''
     local one_line = old_bufinfo.linecount == 1
     local unchanged = old_bufinfo.changed == 0
     if no_name and one_line and unchanged then
-      buf_delete(old_bufnr, {})
+      vim.api.nvim_buf_delete(old_bufnr, {})
     end
   end
 
-  local cur_win = get_current_win()
+  local cur_win = vim.api.nvim_get_current_win()
   if nvim_tree then
-    cmd('NvimTreeOpen')
+    vim.cmd('NvimTreeOpen')
   end
 
   if logger then
@@ -330,8 +291,8 @@ function M.location_window(options)
   }
   options = vim.tbl_extend('keep', options, default_options)
 
-  local bufnr = options.bufnr or buf_create(false, true)
-  local win_id = win_open(bufnr, true, options)
+  local bufnr = options.bufnr or vim.api.nvim_create_buf(false, true)
+  local win_id = vim.api.nvim_open_win(bufnr, true, options)
 
   return {
     bufnr = bufnr,
@@ -340,10 +301,10 @@ function M.location_window(options)
 end
 
 function M.notification(text)
-  local win_stats = list_uis()[1]
+  local win_stats = vim.api.nvim_list_uis()[1]
   local win_width = win_stats.width
 
-  local prev_win = get_current_win()
+  local prev_win = vim.api.nvim_get_current_win()
 
   local info = M.location_window({
     width = 20,
@@ -352,8 +313,8 @@ function M.notification(text)
     col = win_width - 21,
   })
 
-  buf_set_lines(info.bufnr, 0, 5, false, { '!!! Notification', text })
-  set_current_win(prev_win)
+  vim.api.nvim_buf_set_lines(info.bufnr, 0, 5, false, { '!!! Notification', text })
+  vim.api.nvim_set_current_win(prev_win)
 
   return {
     bufnr = info.bufnr,
@@ -362,7 +323,7 @@ function M.notification(text)
 end
 
 function M.close_notification(bufnr)
-  buf_delete(bufnr, { force = true })
+  vim.api.nvim_buf_delete(bufnr, { force = true })
 end
 
 function M.nav_next()
